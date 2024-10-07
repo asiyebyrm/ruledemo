90ccc376756a2d0e63d9f351e20e46f2cf44d554
Asiye <asiyebayram1190@gmail.com>
2024-10-07 21:53:56 +0300

    Refactoring.

--- src/main/java/org/england/ruledemo/config/DroolsService.java
+++ src/main/java/org/england/ruledemo/config/DroolsService.java
@@ -1,16 +1,20 @@
 package org.england.ruledemo.config;
 
 import org.england.ruledemo.model.User;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.repository.Deployment;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DroolsService {
-//    @Autowired
-//    RuntimeService runtimeService;
-//    @Autowired
-//    RepositoryService repositoryService;
+    @Autowired
+    RuntimeService runtimeService;
+    @Autowired
+    RepositoryService repositoryService;
 
     public void executeRule(User user) {
         KieServices kieServices = KieServices.Factory.get();
@@ -20,13 +24,13 @@ public class DroolsService {
         session.dispose();
     }
 
-//    public void deployProcess() {
-//        Deployment deployment = repositoryService.createDeployment()
-//                .addClasspathResource("processes/myProcess.bpmn20.xml")
-//                .deploy();
-//    }
-//
-//    public void startProcess() {
-//        runtimeService.startProcessInstanceByKey("myProcess");
-//    }
+    public void deployProcess() {
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("processes/myProcess.bpmn20.xml")
+                .deploy();
+    }
+
+    public void startProcess() {
+        runtimeService.startProcessInstanceByKey("myProcess");
+    }
 }
